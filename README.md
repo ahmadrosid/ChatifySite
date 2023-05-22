4bc70bee564d10cd852cac7431c4bb0e7a82b0a4
ahmadrosid <alahmadrosid@gmail.com>
2023-05-22 15:03:03 +0700

    chore: tidy up

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Laravel Example project to use openai embedding to build a chatbot from website links.
 
-![demo](demo.png)
+https://github.com/ahmadrosid/ChatifySite/assets/29406408/d244a4cb-1ee6-4f66-b400-590e860f0bf4
--- app/Models/Embedding.php
+++ app/Models/Embedding.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
 use Ramsey\Uuid\Uuid;
 
 class Embedding extends Model
